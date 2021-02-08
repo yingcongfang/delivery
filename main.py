0e31ecc9d446ece72da6d095e5d2654e742261e2
yingcongfang <yf110@duke.edu>
2021-02-08 19:51:56 +0000

    new change

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello Everyone!! Glad to see you! This is a test'
+    return 'Hello Everyone!! Glad to see you!'
 
 @app.route('/name/<value>')
 def name(value):
